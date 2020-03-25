c8db9e1dc98a1e1c5e27d78f5bc2b635c3074440
Daniel Fanelli <daniel.r.fanellijr@gmail.com>
2020-03-25 14:24:42 -0700

    NewPortfolioWebsite
    ASP.NET MVC APP | BOLD AND FRESH

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using PartyRSVP.Models;
+
+namespace PartyRSVP.Controllers {
+    public class HomeController : Controller {
+        public ViewResult Index() {
+            return View("Index");
+        }
+
+         public ViewResult Contact() {
+            return View("Contact");
+        }
+
+        public ViewResult About(){
+            return View("About");
+        }
+
+        public ViewResult Works()
+        {
+            return View("Works");
+        }
+
+        public ViewResult Prices(){
+            return View("Prices");
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse) {
+            // Todo: Store guest response, covered in later lessons
+            return View("Thanks", guestResponse);
+        }
+    }
+}
